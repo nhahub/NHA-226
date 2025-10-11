fadbc704710405edd8252d7c118c19e35c949530
Mohamed-AlAzab <mohamedazb20059@gmail.com>
2025-10-11 13:27:34 +0300

    add: setup

---
+++ lib/app_router.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route? generateRouter(RouteSettings setting) {
+    switch (setting.name) {
+      // case screenName:
+      //   return MaterialPageRoute(builder: (_) => screen());
+    }
+    return null;
+  }
+}
