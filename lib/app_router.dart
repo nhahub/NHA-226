c44cd5b66b3330b542b769ad7ee369d0a94d3f97
Rwan Hossam <rwanhossam0@gmail.com>
2025-10-25 23:58:08 +0300

    search page

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,23 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lingo_sign/search_feature/data_layer/repositories/search_repository.dart';
 import 'package:lingo_sign/search_feature/presentation_layer/bloc/search_bloc.dart';
 import 'package:lingo_sign/search_feature/presentation_layer/screens/profile.dart';
 import 'package:lingo_sign/search_feature/presentation_layer/screens/search_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppRouter {
   Route? generateRouter(RouteSettings setting) {
     switch (setting.name) {
       case '/search':
         return MaterialPageRoute(
-          builder: (context) => BlocProvider.value(
-            value: BlocProvider.of<SearchBloc>(context),
-            child: const SearchScreen(),
+          builder: (_) => FutureBuilder(
+            future: SharedPreferences.getInstance(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const CircularProgressIndicator();
+              final prefs = snapshot.data!;
+              return BlocProvider(
+                create: (_) => SearchBloc(
+                  SearchRepository(FirebaseFirestore.instance, prefs),
+                ),
+                child: const SearchScreen(),
+              );
+            },
           ),
         );
 
       case '/profile':
         return MaterialPageRoute(builder: (_) => const Profile());
+      //   case '/profile/account_info':
+      //     return MaterialPageRoute(builder: (_) => const AccountInfoScreen());
     }
     return null;
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lingo_sign/firebase_options.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-import 'package:lingo_sign/search_feature/data_layer/repositories/search_repository.dart';
-import 'package:lingo_sign/search_feature/presentation_layer/bloc/search_bloc.dart';
 import 'package:lingo_sign/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  final prefs = await SharedPreferences.getInstance();
-  final searchRepository = SearchRepository(FirebaseFirestore.instance, prefs);
-
-  runApp(MyApp(searchRepository: searchRepository));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final SearchRepository searchRepository;
-
-  const MyApp({super.key, required this.searchRepository});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return RepositoryProvider.value(
-      value: searchRepository,
-      child: BlocProvider(
-        create: (_) => SearchBloc(searchRepository),
-        child: ScreenUtilInit(
-          designSize: const Size(375, 812),
-          builder: (context, _) => MaterialApp(
-            debugShowCheckedModeBanner: false,
-            initialRoute: '/search',
-            onGenerateRoute: AppRouter().generateRouter,
-          ),
-        ),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, _) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/search',
+        onGenerateRoute: AppRouter().generateRouter,
       ),
     );
   }
