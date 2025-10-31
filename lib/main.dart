c69bfde2d0d02eccf4b5583dc9c15da96ab51403
Rwan Hossam <rwanhossam0@gmail.com>
2025-10-31 22:21:08 +0200

    searchPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lingo_sign/firebase_options.dart';
 import 'package:lingo_sign/app_router.dart';
+import 'package:lingo_sign/search_feature/data_layer/repositories/search_repository.dart';
+import 'package:lingo_sign/search_feature/presentation_layer/bloc/search_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final searchRepository = SearchRepository(firestore, prefs);
 
-  runApp(MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => SearchBloc(searchRepository)),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
