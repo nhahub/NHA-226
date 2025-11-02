b15f5b52478ae52aef9ebfdf04ee72ac4c19901e
Mohamed AlAzab <mohamedazb200559@gmail.com>
2025-11-02 23:34:55 +0200

    Delete lib/features/home directory

--- lib/features/home/domain/entities/user_app.dart
+++
@@ -1,6 +0,0 @@
-class UserApp {
-  final String name;
-  final String imageUrl;
-
-  UserApp({required this.name, required this.imageUrl});
-}
--- lib/features/home/domain/entities/user_favourite.dart
+++
@@ -1,6 +0,0 @@
-class UserFavourite {
-  final String name;
-  final String imageUrl;
-
-  UserFavourite({required this.name, required this.imageUrl});
-}
